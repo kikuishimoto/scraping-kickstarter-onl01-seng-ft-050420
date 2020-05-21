923cda3ad4e4e634fa2a058f9bfba6842a4cae01
kiku ishimoto <kikuish@gmail.com>
2020-05-21 22:35:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,13 +1,10 @@
-# require libraries/modules here
 require "nokogiri"
 require "pry"
+ 
 def create_project_hash
-  # write your code here
-  html = File.read('fixtures/Kickstarter.html')
-
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
   binding.pry
 end
-
+ 
 create_project_hash
